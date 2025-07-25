964b17b2a25e8c5ca2df0f326db49c310a5e2a18
Coded with Avik <148305573+Avik1152@users.noreply.github.com>
2025-07-25 12:25:14 +0000

    concertionOfDatatypes

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,32 @@
+let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof score)
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1
+false => 0
+*/
+
+// let isLoggedIn = 1
+// let isLogged = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// let booleanIsLogged = Boolean(isLogged)
+// console.log(booleanIsLogged)
+
+// 1 => true; 0=>false
+// "" => false
+// "Avik" => true
+
+let someNumber = 33;
+
+let stringNum = String(someNumber)
+console.log(stringNum)
+console.log(typeof stringNum)
