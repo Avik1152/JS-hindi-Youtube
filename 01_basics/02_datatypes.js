86933626cf59a5bc55a6aee77af77402780e6bb3
Coded with Avik <148305573+Avik1152@users.noreply.github.com>
2025-07-25 10:00:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alurt(3+3) -> we are useing nodeJS,not browser
+
+console.log(3
+    +3) //code readability should be high
+
+console.log("avik")
+
+let name = "Avik"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//BigInt
+//string => ""
+//boolean => true / false
+//null => standalone Value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //object
+
